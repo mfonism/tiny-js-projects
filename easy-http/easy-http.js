24adb8a4a20fe533d2d1ec39eafb09531652bfb2
Mfon Eti-mfon <mfonetimfon@gmail.com>
2021-09-02 12:04:07 +0100

    (easy-http) Use a newly instantiated xhr object for each http method invocation
    So methods can be invoked together in the same code

--- easy-http/easy-http.js
+++ easy-http/easy-http.js
@@ -1,11 +1,17 @@
 function EasyHTTP() {
-    this.http = new XMLHttpRequest();
+
+}
+
+EasyHTTP.prototype.getHttpClient = function () {
+    return new XMLHttpRequest()
 }
 
 EasyHTTP.prototype.get = function (url, callback) {
-    this.http.open('GET', url, true);
-    this.http.onload = function (event) {
-        // inside the handler the `this` keyword refers to the
+    const http = this.getHttpClient();
+
+    http.open('GET', url, true);
+    http.onload = function (event) {
+        // inside this handler the `this` keyword refers to the
         // xmlhttp object
         // unless we're using an arrow function
         // where `this` will be whatever it is in the outer scope
@@ -15,44 +21,50 @@ EasyHTTP.prototype.get = function (url, callback) {
             callback(json = null, error = 'Error: ' + this.status);
         }
     };
-    this.http.send();
+    http.send();
 };
 
 EasyHTTP.prototype.post = function (url, data, callback) {
-    this.http.open('POST', url, true);
-    this.http.setRequestHeader('Content-type', 'application/json');
-    this.http.onload = function (event) {
+    const http = this.getHttpClient();
+
+    http.open('POST', url, true);
+    http.setRequestHeader('Content-type', 'application/json');
+    http.onload = function (event) {
         if (this.status === 201) {
             callback(json = JSON.parse(this.responseText), error = null);
         } else {
             callback(json = null, error = 'Error: ' + this.status);
         }
     }
-    this.http.send(JSON.stringify(data));
+    http.send(JSON.stringify(data));
 };
 
 EasyHTTP.prototype.put = function (url, data, callback) {
-    this.http.open('PUT', url, true);
-    this.http.setRequestHeader('Content-type', 'application/json');
-    this.http.onload = function (event) {
+    const http = this.getHttpClient();
+
+    http.open('PUT', url, true);
+    http.setRequestHeader('Content-type', 'application/json');
+    http.onload = function (event) {
         if (this.status === 200) {
             callback(json = JSON.parse(this.responseText), error = null);
         } else {
             callback(json = null, error = 'Error: ' + this.status);
         }
     }
-    this.http.send(JSON.stringify(data));
+    http.send(JSON.stringify(data));
 };
 
 EasyHTTP.prototype.delete = function (url, callback) {
-    this.http.open('DELETE', url, true);
-    this.http.setRequestHeader('Content-type', 'application/json');
-    this.http.onload = function (event) {
+    const http = this.getHttpClient();
+
+    http.open('DELETE', url, true);
+    http.setRequestHeader('Content-type', 'application/json');
+    http.onload = function (event) {
         if (this.status === 200) {
             callback(json = JSON.parse(this.responseText), error = null);
         } else {
             callback(json = null, error = 'Error: ' + this.status);
         }
     }
-    this.http.send();
+    http.send();
 }
